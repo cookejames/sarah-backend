97d6c3f2dd3b60263609078367a888bc0b3ab1d4
James Cooke <james@cookeit.co.uk>
2015-12-14 12:06:06 +0000

    Added on/off RPC routes to send the on/off commands to a switch

---
+++ common/models/switch.js
@@ -0,0 +1,47 @@
+module.exports = function(Switch) {
+  Switch.remoteMethod('sendOn', {
+    description: 'Sends the on command to this switch',
+    accepts: [
+      {arg: 'req', type: 'object', 'http': {source: 'req'}},
+      {arg: 'id', type: 'string'}
+    ],
+    returns: {arg: 'data', type: this.modelName, root: true},
+    http: {verb: 'post', path: '/:id/on'}
+  });
+  Switch.sendOn = function(req, id, cb) {
+    Switch.findById(id, function(err, theSwitch) {
+      if (err) {
+        return cb(err);
+      }
+      if (!theSwitch) {
+        return cb({name: 'Not Found', status: 404, message: 'Entity not found'});
+      }
+      var mqtt = require('../../server/lib/mqtt.js').mqtt(req.app.get('mqtt'));
+      mqtt.publish('switches', String(theSwitch.onCommand));
+      cb();
+    });
+  };
+
+  Switch.remoteMethod('sendOff', {
+    description: 'Sends the off command to this switch',
+    accepts: [
+      {arg: 'req', type: 'object', 'http': {source: 'req'}},
+      {arg: 'id', type: 'string'}
+    ],
+    returns: {arg: 'data', type: this.modelName, root: true},
+    http: {verb: 'post', path: '/:id/off'}
+  });
+  Switch.sendOff = function(req, id, cb) {
+    Switch.findById(id, function(err, theSwitch) {
+      if (err) {
+        return cb(err);
+      }
+      if (!theSwitch) {
+        return cb({name: 'Not Found', status: 404, message: 'Entity not found'});
+      }
+      var mqtt = require('../../server/lib/mqtt.js').mqtt(req.app.get('mqtt'));
+      mqtt.publish('switches', String(theSwitch.offCommand));
+      cb();
+    });
+  };
+};
