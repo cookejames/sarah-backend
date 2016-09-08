52cc7ad81fa84129e622b00bed70953bb5488cfa
James Cooke <james@cookeit.co.uk>
2016-09-08 08:18:56 +0100

    Changed MQTT client

--- server/lib/mqtt.js
+++ server/lib/mqtt.js
@@ -1,12 +1,34 @@
-(function(){
-  var mqtt;
+var mqtt = require('mqtt');
+var client;
 
-  exports.mqtt = function(config) {
-    if (!mqtt) {
-      var Adapter = require('strong-pubsub');
-      var Client = require('strong-pubsub-mqtt');
-      mqtt = new Adapter(config, Client);
-    }
-    return mqtt;
-  };
-})();
+exports.mqtt = function(config) {
+  if (!client) {
+    client = mqtt.connect(
+      'mqtt://' + config.host + ':' + config.port,
+      {
+        username: config.mqtt.username,
+        password: config.mqtt.password,
+        will: {
+          topic: 'server/connected',
+          payload: 'false',
+          qos: 0,
+          retain: true
+        }
+      }
+    );
+
+    client.on('connect', function () {
+      console.log(new Date().toLocaleString(), '- MQTT Connected');
+      client.publish('server/connected', 'true', {retain: true});
+    });
+
+    client.on('offline', function () {
+      console.log(new Date().toLocaleString(), '- MQTT Client Offline ');
+    });
+
+    client.on('error', function () {
+      console.log(new Date().toLocaleString(), '- MQTT Error');
+    });
+  }
+  return client;
+};
