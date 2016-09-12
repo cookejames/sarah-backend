216135a2e288d6b88fabf3a5e03a98c77d379d30
James Cooke <james@cookeit.co.uk>
2016-09-12 09:30:23 +0100

    Update inject to not break login

--- server/boot/inject-context.js
+++ server/boot/inject-context.js
@@ -1,37 +1,50 @@
 module.exports = function (app) {
-  function inject(ctx, next) {
-    var options = hasOptions(ctx.method.accepts) && (ctx.args.options || {});
-    if(options) {
-      options.accessToken = ctx.req.accessToken;
-      ctx.args.options = options;
+  /*
+   * Sets up options injection for passing accessToken and other properties around internally
+   */
+  var setupOptionsInjection = function () {
+    function inject(ctx, next) {
+      var options = hasOptions(ctx.method.accepts) && (ctx.args.options || {});
+      if (options) {
+        options.accessToken = ctx.req.accessToken;
+        ctx.args.options = options;
+      }
+      next();
     }
-    next();
-  }
 
-  app.remotes().before('*.*', inject);
-
-  app.remotes().before('*.prototype.*', function(ctx, instance, next) {
-    inject(ctx, next);
-  });
-
-  // unfortunately this requires us to add the options object
-  // to the remote method definition
-  app.remotes().methods().forEach(function(method) {
-    if(!hasOptions(method.accepts)) {
-      method.accepts.push({
-        arg: 'options',
-        type: 'object',
-        injectCtx: true
-      });
-    }
-  });
-
-  function hasOptions(accepts) {
-    for (var i = 0; i < accepts.length; i++) {
-      var argDesc = accepts[i];
-      if (argDesc.arg === 'options' && argDesc.injectCtx) {
-        return true;
+    function hasOptions(accepts) {
+      for (var i = 0; i < accepts.length; i++) {
+        var argDesc = accepts[i];
+        if (argDesc.arg === 'options' && argDesc.injectCtx) {
+          return true;
+        }
       }
     }
-  }
+
+    if (!process.env.GENERATING_SDK) {
+      app.remotes().before('*.*', inject);
+
+      app.remotes().before('*.prototype.*', function (ctx, instance, next) {
+        if (typeof instance === 'function') {
+          next = instance;
+        }
+        inject(ctx, next);
+      });
+
+      var blacklist = ['login', 'logout', 'confirm', 'resetPassword'];
+
+      // unfortunately this requires us to add the options object
+      // to the remote method definition
+      app.remotes().methods().forEach(function (method) {
+        if (!hasOptions(method.accepts) && blacklist.indexOf(method.name) === -1) {
+          method.accepts.push({
+            arg: 'options',
+            description: '**Do not implement in clients**',
+            type: 'object',
+            injectCtx: true
+          });
+        }
+      });
+    }
+  };
 };
